3a0ad5e5f79e5687ffe507befd87f01b6eab4565
Klibar <ma8964032@gmail.com>
2026-04-14 18:39:16 +0200

    second task

---
+++ TaskTwo.dart
@@ -0,0 +1,6 @@
+// 2.Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+void main() {
+  String name = 'John Doe';
+  print("hello I'm ${name}");
+  print('hello I am "${name}"');
+}
