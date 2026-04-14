233417272b9aec459ba355527165a5822acdee6d
Klibar <ma8964032@gmail.com>
2026-04-14 18:46:26 +0200

    forth task

---
+++ TaskFour.dart
@@ -0,0 +1,7 @@
+// 4.Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+void main() {
+  double formula;
+  var p, t, r;
+  formula = (p * t * r) / 100;
+  print(formula);
+}
